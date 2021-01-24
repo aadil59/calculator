eed7f377a647e87dad9eab771a624d59886ac145
Aadil Khan <Tech@Aadils-MacBook-Pro-Dev.local>
2021-01-25 00:27:10 +0530

    Updates

---
+++ precache-manifest.e8211d3b93ef42e47917665cc006c5e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a3c08c3e9e7634e6596032a88c17e8",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "fc866f3f8296d223b66e",
+    "url": "/calculator/static/css/main.2178b985.chunk.css"
+  },
+  {
+    "revision": "7b26f6912c3ac2c1318e",
+    "url": "/calculator/static/js/2.fed42e98.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/calculator/static/js/2.fed42e98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc866f3f8296d223b66e",
+    "url": "/calculator/static/js/main.e62b1215.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
